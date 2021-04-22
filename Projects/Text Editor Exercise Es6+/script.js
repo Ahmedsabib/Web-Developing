875ae9c22c8ccdbf2540939772e2854e6ecbcb79
Ahmedsabib <67859165+Ahmedsabib@users.noreply.github.com>
2021-04-22 14:35:19 +0600

    Add files via upload

---
+++ Projects/Text Editor Exercise Es6+/script.js
@@ -0,0 +1,33 @@
+updateText = () =>{
+    let text = document.getElementById("text-input").value;
+    document.getElementById("text-output").innerText = text;
+}
+
+makeBold = (elem)=>{
+    elem.classList.toggle('active');
+    document.getElementById("text-output").classList.toggle('bold'); 
+}
+
+makeItalic = (elem) =>{
+    elem.classList.toggle('active');
+    document.getElementById('text-output').classList.toggle('italic');
+}
+
+makeUnderline = (elem) =>{
+    elem.classList.toggle('undeline');
+    let formattedtext = document.getElementById('text-output');
+    if(formattedtext.classList.contains('underline')){
+        formattedtext.classList.remove('underline');
+    } else{
+        formattedtext.classList.add('underline');
+    }
+}
+
+alignText = (elem, alignType) =>{
+    document.getElementById('text-output').style.textAlign = alignType;
+    let alignButton = document.getElementsByClassName('align');
+    for(let button of alignButton){
+        button.classList.remove('active');
+    }
+    elem.classList.toggle('active');
+}
