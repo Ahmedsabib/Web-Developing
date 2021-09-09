c3490b1ef9023c72ae2e6b65c76141ab7e61cd15
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2021-09-09 14:42:57 +0600

    Add files via upload

---
+++ Projects/Color Switcher/script.js
@@ -0,0 +1,9 @@
+function backgroundRed() {
+    document.body.style.backgroundColor = "red";
+}
+function backgroundBlue() {
+    document.body.style.backgroundColor = "blue";
+}
+function backgroundGreen() {
+    document.body.style.backgroundColor = "green";
+}
